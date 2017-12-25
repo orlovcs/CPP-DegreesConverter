bea5e7cf6aeb5bfc8900599ee9084afb60e5213d
Nikita <orlov.a.nikita@gmail.com>
2017-12-25 00:02:52 -0500

    done

--- main.cpp
+++ main.cpp
@@ -4,11 +4,18 @@ int main(int argc, const char * argv[]) {
     // insert code here...
     const int numArgs = argc - 1;
     if (numArgs>0 && strcmp(argv[0],"-f")){
-        std::cout << "Enter Farenheit" << std::endl;
-        
+        std::cout << "Enter farenheit" << std::endl;
+        double f;
+        std::cin >> f;
+        std::cout << f << " farenheit in celsius is " << (f - 32)/1.8<< std::endl;
+
     }else{
         std::cout << "Enter Celsuis" << std::endl;
-        
+        double c;
+        std::cin >> c;
+        std::cout << c << " celsius in farenheit is " << c * 1.8 + 32<< std::endl;
+
     }
+    return 0;
 }
 
